cc9403610dba6baaa969466823474a334756d409
Sirvess <joe.slask@gmail.com>
2020-11-24 22:25:46 +0100

    clarify algorithm

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ from testData import testInput
 # for i in testInput:
 #     print(i)
 
-# --- DFS ---
-
-# Initialize backlog with
+# Initialize backlog
 backlog = matrix[0];
 
-# Loop through backlog until everything is explored
+# Search
+# Algorithm:
+# For empty cells
+# 1. Suggest 1
+# 2. Check if valid
+# 3. If valid - go to next
+# 4. If not valid - go back to previous unknown.
+# 5. If previous unknown is already 9 - go to previous unknown. If on last unknown and already at 9 - No solution.
+# 6. Increment previous unknown with 1.
+# 7. Repeat
 
 # Print solved matrix
